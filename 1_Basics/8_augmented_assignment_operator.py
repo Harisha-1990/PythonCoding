612a8db5be29916fe660f03067885f890c3409b1
Harisha-1990 <harisha.1990@gmail.com>
2021-01-09 07:40:00 -0500

    Create 8_augmented_assignment_operator.py

---
+++ 1_Basics/8_augmented_assignment_operator.py
@@ -0,0 +1,9 @@
+#author : Harisha
+#augmented assignment operator
+some_value = 5
+some_value += 2 #7
+some_value*=2 #14
+print(some_value)
+
+#augumented assignment operator is a short hand form os saying some_value = some_value + 2
+#opeartor comes to the left of the equal sign
