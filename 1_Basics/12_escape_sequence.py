5b56aa33469decdb88914a9b6a83fae157551e80
Harisha-1990 <harisha.1990@gmail.com>
2021-01-09 08:21:15 -0500

    Create 12_escape_sequence.py

---
+++ 1_Basics/12_escape_sequence.py
@@ -0,0 +1,19 @@
+#author : Harisha
+#escape sequence
+#escape sequence is a backslash \ that lets python know whatever comes after this is a string
+weather = 'It\'s kind of sunny'
+print(weather)
+weather = 'It\'s "kind of" sunny.'
+print(weather)
+# if we want to have a actual backslash
+
+weather = 'It\'\s kind of funny'
+print(weather)
+#\t
+# it indicates whatever comes after backslash has a tab
+weather = 'It\t\'s kind of sunny'
+print(weather)
+#\n 
+# it indicates watever comes after backslash has a new line
+weather = 'It\'s kind of sunny \nhope you have a good day'
+print(weather)
