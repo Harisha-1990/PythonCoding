9ca4c0250a36a38798c355d3be0f81ffbf7bc94d
Harisha-1990 <harisha.1990@gmail.com>
2021-01-09 16:21:40 -0500

    Create 18_type_conversion_calculations.py

---
+++ 1_Basics/18_type_conversion_calculations.py
@@ -0,0 +1,15 @@
+#author : Harisha
+#Exericse Type conversion
+name = 'Harisha'
+age = 50
+relationship_status = 'Single'
+relationship_status = 'It\'s complicated'
+print(relationship_status)
+#calculate age using birth year
+birth_year = input('What year were you born?\n')
+age = 2021-int(birth_year)
+print(f'your age is {age}')
+age = 2021-float(birth_year)
+print(f'your age is {age}')
+age = 2021-bool(birth_year)
+print(f'your age is {age}')
