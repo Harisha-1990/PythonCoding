7660651750b9b0663add6873d8ece746f95d059e
Harisha-1990 <harisha.1990@gmail.com>
2021-01-09 07:55:54 -0500

    Create 9_strings.py

---
+++ 1_Basics/9_strings.py
@@ -0,0 +1,15 @@
+#author : Harisha
+#strings
+#string is a piece of text that os written in a quotation marks or double quotes
+print(type("hi there"))
+print(type('hi there'))
+#long sring has three single quotes. By using three single quotes we can do multi lines strings
+long_string= '''
+WOW
+0 0
+'''
+print(long_string)
+first_name= 'Harisha'
+last_name = 'Vennapusa'
+full_name = first_name+' '+last_name
+print(full_name)
