dbf0b90450fa277ccba10e06d79a56eaa330031c
Harisha-1990 <harisha.1990@gmail.com>
2021-01-09 18:34:00 -0500

    Create 24_matrix.py

---
+++ 1_Basics/24_matrix.py
@@ -0,0 +1,15 @@
+#author : Harisha
+#matrix
+#It's a way for us to descibe 2D lists or multi dimensional lists. 
+#It's an array with another array inside of it
+#Why is matrix useful? 
+#matrices come up a lot in topics like machine learning or image processing
+#let's say we have an image of 'X', a compute can understand only image based on pixels on the screen'
+matrix =[
+  [1,2,3],
+  [2,4,6],
+  [7,8,9]
+]
+print(matrix[0][1]) # [0]is acessing the first list and [1] is acessing the first item in it
+print(matrix[2][2])
+print(matrix[0][1])
