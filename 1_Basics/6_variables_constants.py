27592a5150dd36284d393742949d40dcbdf436f1
Harisha-1990 <harisha.1990@gmail.com>
2021-01-09 07:26:06 -0500

    Create 6_variables_constants.py

---
+++ 1_Basics/6_variables_constants.py
@@ -0,0 +1,30 @@
+#varaiables are ways for us to store information
+# assigning the value to a variable
+#binding the value to a variable
+var1 = 10
+user_age = var1/4
+a= user_age
+b,c,d= 1,2,3 #rapidly assigning values to the varaibles
+
+print(var1)
+print(user_age)
+print(a)
+print(b,c,d)
+print(c)
+# Good practices to write variables
+# snake_case
+# start with lowercase or underscore
+# letteres, numbers, underscores
+# case sensitive
+# don't overwrite keywords
+
+# constants 
+# cosntants are not going to change throughout the program.
+# indicate constants in Upper case
+# if we want we can change and reassign to different value
+#but good practice is not to change constants
+PI = 3.14
+
+ #dunder varibales
+ # they are with double underscores __
+ # we should not create a regular variable with two underscores __ and assign a value to it
