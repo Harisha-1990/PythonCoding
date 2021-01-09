9c9e0ac6c76a8a24c18bea7f2efa561a88f191f1
Harisha-1990 <harisha.1990@gmail.com>
2021-01-09 16:50:08 -0500

    Create 20_lists.py

---
+++ 1_Basics/20_lists.py
@@ -0,0 +1,15 @@
+#author : Harisha
+#lists
+#lists are the collection of items such as integers, strings, booleans etc.,
+#are denoted with sqaure brackets
+#In python lists are the form of arrays
+li =[1,2,3,4,5]
+li2=['a','b','c']
+li3=[1,2,'a',True]
+#Data structure: It's a way for us to organize information and data into a folder or a box. 
+#Exmple fridge: It's where you store all the food items. you take them out whenever required. 
+#example backpack: good to put items in it. 
+amazon_cart = ['notebooks','sunglasses']
+print(amazon_cart[0])
+print(amazon_cart[1])
+print(amazon_cart[2]) # index out of range
