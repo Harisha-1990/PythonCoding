28faa2e9ba5ba168d5a759b6620a421a8804f90f
Harisha-1990 <harisha.1990@gmail.com>
2021-01-10 09:17:14 -0500

    Create 30_exer_common_list_patterns.py

---
+++ 1_Basics/30_exer_common_list_patterns.py
@@ -0,0 +1,61 @@
+#fix this code so that it prints a sorted list of all of our friends (alphabetical). Scroll to see answer
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+# new_friend = ['Stanley']
+
+# print(friends.sort() + new_friend)
+
+friends.append('Stanley')
+friends.sort()
+print(friends)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solution: (keep in mind there are multiple ways to do this, so your answer may vary. As long as it works that's all that matters!)
+# friends.extend(new_friend)
+# print(sorted(friends))
+
+
