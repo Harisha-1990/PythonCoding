ea53a6028e355f980eec54fd4b4d1bd31e089275
Harisha-1990 <harisha.1990@gmail.com>
2021-01-10 09:23:10 -0500

    Create 32_None_datatype.py

---
+++ 1_Basics/32_None_datatype.py
@@ -0,0 +1,11 @@
+#author: Harisha
+#None
+#Some methods we worked on lists didn't really produce anything
+#even if we modified the lists in place the output was None
+
+#None 
+#It's a special datatype exists in python which represents the absence of value in most languges 
+#In someother languages null represents the absence of value
+
+weapons= None # this user doesn't really have any weapons
+print(weapons)
