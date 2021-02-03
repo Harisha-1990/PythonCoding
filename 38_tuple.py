c9fe110f2d7ad2f3e2d2abcd046a00d485405845
Harisha-1990 <harisha.1990@gmail.com>
2021-02-02 19:26:40 -0500

    Create 38_tuple.py

---
+++ 38_tuple.py
@@ -0,0 +1,10 @@
+my_tuple = (1,2,3,4,5)
+print(my_tuple[1]) # just like a list we can access tuple through index
+print(5 in my_tuple)
+user ={
+(1,2):[1,2,3]   #we're trying to use tuple inside dictionary
+,'greet':'hello'
+,'age':20
+
+}
+print(user[(1,2)])
